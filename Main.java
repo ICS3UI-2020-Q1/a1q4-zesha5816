3883dca62c7ed73c3a3714916d7639b529d4c4c0
zesha5816 <zesha5816@wrdsb.ca>
2020-09-14 15:56:29 +0000

    finished the example question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *a program to help grade 9s calculate the slope of a line given two points
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+		Scanner input = new Scanner(System.in);
+		//prompt user to input the x1 co ordinate
+  System.out.println("Please enter in the x coordinate of the first point");
+	//get input from user for x1 
+	int x1 = input.nextInt();
+	// rpomt user for y1
+	System.out.println("Please enter in the y coordinate of the first point");
+	int y1 = input.nextInt();
+  System.out.println("Please enter in the x coordinate of the second point");
+	int x2 = input.nextInt();
+	System.out.println("Please enter in the y coordinate of the second point");
+	int y2 = input.nextInt();
+	//calc the top half of the slope form
+	double top_half = y2 - y1;
+	//calc the bottom half of the slope form
+	double bottom_half = x2 - x1;
+	// divide the top half and the bottom half 
+	double slope = top_half/bottom_half;
+	// print the slope 
+	System.out.println("The slope of your line would be " + slope + ".");
   }
 }
